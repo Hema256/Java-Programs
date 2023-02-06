748cb1a0f04948cf10b0c634246ae309b8575232
Hema256 <84697657+Hema256@users.noreply.github.com>
2023-02-06 23:01:53 +0530

    Add files via upload

---
+++ ab.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class ab
+{
+    public static void main(String args[])
+    {
+      String s="hema,alekhya,sravanthi";
+      System.out.println(s);
+      String fn=s.substring(beginIndex(0),data.indexOf(",");
+      System.out.print("first name"+fn);
+      string 
+       
+    }
+   
+}
